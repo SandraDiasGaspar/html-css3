4b8985c868c498abbf4622129d687498da796e67
Sandra Gaspar <sandraalvela@gmail.com>
2024-07-06 10:20:20 +0100

    upgrade

--- formularioatlantida/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDiv(show) {
-    constvar opcaoclt = document.getElementById('opcaoclt');
-    if (show) {
-        extraFields.style.display = 'block';
-    } else {
-        extraFields.style.display = 'none';
-    }
-}
